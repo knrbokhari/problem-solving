cd9f0b4ead60a0c1f4337b78a5085ad7b1410e62
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-12-06 00:05:29 +0600

    first commit

---
+++ coderbyte.com/React_Button_Toggle.js
@@ -0,0 +1,18 @@
+// import React, { useState } from 'react';
+// import ReactDOM from 'react-dom';
+
+// function Toggle() {
+//   const [button, setButton] = useState(true)
+//   function handleClick() {
+//     setButton(!button)
+//   }
+  
+//   return (
+//     <button onClick={e => handleClick()}>{button ? 'ON' : 'OFF'}</button>
+//   );
+// }
+
+// ReactDOM.render(
+//   <Toggle />,
+//   document.getElementById('root')
+// );
