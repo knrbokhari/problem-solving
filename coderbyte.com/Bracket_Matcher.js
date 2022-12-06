e93d2067a7cd02d2440c69a016343b331df14f17
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-12-06 15:48:10 +0600

    Bracket Matcher

---
+++ coderbyte.com/Bracket_Matcher.js
@@ -0,0 +1,36 @@
+// Bracket Matcher
+// Have the function BracketMatcher(str) take the str parameter being passed and return 1 if the brackets are correctly matched and each one is accounted for. Otherwise return 0. For example: if str is "(hello (world))", then the output should be 1, but if str is "((hello (world))" the the output should be 0 because the brackets do not correctly match up. Only "(" and ")" will be used as brackets. If str contains no brackets return 1.
+// Examples
+// Input: "(coder)(byte))"
+// Output: 0
+// Input: "(c(oder)) b(yte)"
+// Output: 1
+
+function BracketMatcher(str) { 
+
+    let strArray = str.split('')
+    let count = 0
+
+    for (let i = 0; i < strArray.length; i++) {
+        if(strArray[i] === '(') {
+            count += 1
+        }
+
+        if(strArray[i] === ")"){
+            count -= 1
+        } 
+        
+        if(count < 0){
+            return 0
+        }
+    }
+    
+    if(count !== 0) return 0;
+
+    return 1;
+}
+     
+// keep this function call here 
+console.log(BracketMatcher("(coder)(byte))"));
+console.log(BracketMatcher("(c(oder)) b(yte)"));
+console.log(BracketMatcher("the color re(d))()(()"));
