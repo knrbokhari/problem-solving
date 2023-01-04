b8b729f0abad75b7d93bed0949c986b0548cf03b
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-04 20:18:54 +0600

    20._Valid_Parentheses Solution leetcode

---
+++ leetcode.com/Easy/ 20._Valid_Parentheses.js
@@ -0,0 +1,30 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+const isValid = (s) => {
+    const stack = [];
+    const map = new Map([
+        ['(', ')'],
+        ['[', ']'],
+        ['{', '}']
+    ]);
+
+    for (let i = 0 ; i < s.length ; i++) {
+        if (map.has(s[i])) {
+            stack.push(map.get(s[i]));
+        } else if (s[i] !== stack.pop()) {
+            return false;
+        } 
+    }
+    return stack.length === 0;
+};
+
+console.log(isValid("()"))
+console.log(isValid("()[]{}"))
+console.log(isValid("(]"))
+console.log(isValid("([)]"));
