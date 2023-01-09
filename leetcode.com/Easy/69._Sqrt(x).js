8585f276d9d891d7148a1c204263e70fd663eef1
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-01-09 20:37:18 +0600

    69._Sqrt(x) Solution leetcode

---
+++ leetcode.com/Easy/69._Sqrt(x).js
@@ -0,0 +1,14 @@
+// 69. Sqrt(x)
+
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+const mySqrt = (x) => {
+    return parseInt(x**0.5)
+};
+
+console.log(mySqrt(4))
+console.log(mySqrt(8))
